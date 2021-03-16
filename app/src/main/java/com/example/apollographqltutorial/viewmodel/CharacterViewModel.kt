143d841d221663cafa9f6ab04c67a8f28ff8e293
Fahri MacBook <fahri-can@web.de>
2021-03-16 20:57:01 +0100

    Implement ViewModel.

--- app/src/main/java/com/example/apollographqltutorial/viewmodel/CharacterViewModel.kt
+++ app/src/main/java/com/example/apollographqltutorial/viewmodel/CharacterViewModel.kt
@@ -1,9 +1,18 @@
 package com.example.apollographqltutorial.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
+import com.example.apollographqltutorial.CharactersListQuery
 import com.example.apollographqltutorial.repository.CharacterRepository
+import com.example.apollographqltutorial.view.state.ViewState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
@@ -13,6 +22,19 @@ class CharacterViewModel @Inject constructor(
     private val repository: CharacterRepository,
 ) : ViewModel() {
 
+    private val _charactersList by lazy { MutableLiveData<ViewState<Response<CharactersListQuery.Data>>>() }
+    val charactersList: LiveData<ViewState<Response<CharactersListQuery.Data>>>
+        get() = _charactersList
 
+    fun queryCharactersList() = viewModelScope.launch {
+        _charactersList.postValue(ViewState.Loading())
+        try {
+            val response = repository.queryCharactersList()
+            _charactersList.postValue(ViewState.Success(response))
+        } catch (e: ApolloException) {
+            Log.d("ApolloException", "Failure", e)
+            _charactersList.postValue(ViewState.Error("Error fetching characters"))
+        }
+    }
 
 }
