3a7866b5f4683239c376e8a9883e1767af170f2a
Fahri MacBook <fahri-can@web.de>
2021-05-13 00:48:58 +0200

    Polish tests.

--- app/src/test/java/com/example/apollographqltutorial/repository/CharacterRepositoryImplTest2.kt
+++ app/src/test/java/com/example/apollographqltutorial/repository/CharacterRepositoryImplTest2.kt
@@ -2,20 +2,14 @@ package com.example.apollographqltutorial.repository
 
 import com.apollographql.apollo.ApolloCall
 import com.apollographql.apollo.ApolloClient
-import com.apollographql.apollo.ApolloQueryCall
 import com.apollographql.apollo.api.Response
 import com.apollographql.apollo.exception.ApolloException
 import com.example.apollographqltutorial.CharactersListQuery
-import com.example.apollographqltutorial.MainCoroutineRule
 import com.example.apollographqltutorial.repository.BaseRepository.Companion.GENERAL_ERROR_CODE
 import com.example.apollographqltutorial.repository.BaseRepository.Companion.SOMETHING_WRONG
 import com.example.apollographqltutorial.util.ResponseFileReader
 import com.example.apollographqltutorial.view.state.ViewState
 import com.google.gson.Gson
-import io.mockk.MockKAnnotations
-import io.mockk.coEvery
-import io.mockk.impl.annotations.RelaxedMockK
-import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import okhttp3.mockwebserver.MockResponse
@@ -23,7 +17,6 @@ import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
@@ -32,9 +25,6 @@ import org.junit.runners.JUnit4
 @ExperimentalCoroutinesApi
 @RunWith(JUnit4::class)
 class CharacterRepositoryImplTest2 {
-
-    @get:Rule
-    var mainCoroutineRule = MainCoroutineRule()
 
     private val mockWebServer = MockWebServer()
 
@@ -78,29 +68,18 @@ class CharacterRepositoryImplTest2 {
     }
 
     @Test
-    fun `given response cahracters list when fetching results then return success`() {
+    fun `given response characters list when fetching results then return success`() {
         val reader = ResponseFileReader("characters_list_success.json")
         val gson = Gson()
         val expected: CharactersListQuery.Data =
             gson.fromJson(reader.content, CharactersListQuery.Data::class.java)
 
-
-        mockWebServer.apply {
-            enqueue(
-                MockResponse().setResponseCode(200)
-                    .setBody(ResponseFileReader("characters_list_success.json").content)
-            )
-        }
-
         runBlocking {
-
-            var actual: CharactersListQuery.Data? = null
 
             mockApi.query(CharactersListQuery())
                 .enqueue(object : ApolloCall.Callback<CharactersListQuery.Data>() {
                     override fun onResponse(response: Response<CharactersListQuery.Data>) {
-                        actual = response.data
-                        assertEquals(expected, actual)
+                        assertEquals(expected, response.data)
                     }
 
                     override fun onFailure(e: ApolloException) {}
