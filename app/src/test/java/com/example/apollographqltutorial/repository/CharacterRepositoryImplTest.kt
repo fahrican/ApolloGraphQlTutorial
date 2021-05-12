db1199527123ae509e80317f1b0a5f617590b187
Fahri MacBook <fahri-can@web.de>
2021-05-12 22:22:27 +0200

    Remove unnecessary code.

--- app/src/test/java/com/example/apollographqltutorial/repository/CharacterRepositoryImplTest.kt
+++ app/src/test/java/com/example/apollographqltutorial/repository/CharacterRepositoryImplTest.kt
@@ -1,24 +1,13 @@
 package com.example.apollographqltutorial.repository
 
-import com.apollographql.apollo.ApolloClient
 import com.example.apollographqltutorial.CharactersListQuery
-import com.example.apollographqltutorial.MainCoroutineRule
-import com.example.apollographqltutorial.repository.BaseRepository.Companion.GENERAL_ERROR_CODE
-import com.example.apollographqltutorial.repository.BaseRepository.Companion.SOMETHING_WRONG
-import com.example.apollographqltutorial.util.ResponseFileReader
 import com.example.apollographqltutorial.view.state.ViewState
-import com.google.gson.Gson
 import io.mockk.*
 import io.mockk.impl.annotations.RelaxedMockK
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.runBlockingTest
-import okhttp3.mockwebserver.MockResponse
-import okhttp3.mockwebserver.MockWebServer
-import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
@@ -26,9 +15,6 @@ import org.junit.runners.JUnit4
 @ExperimentalCoroutinesApi
 @RunWith(JUnit4::class)
 class CharacterRepositoryImplTest {
-
-    @get:Rule
-    var mainCoroutineRule = MainCoroutineRule()
 
     @RelaxedMockK
     private lateinit var mockData: CharactersListQuery.Data
