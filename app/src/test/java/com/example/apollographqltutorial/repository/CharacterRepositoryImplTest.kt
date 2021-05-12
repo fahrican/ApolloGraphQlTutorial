5bc42b6e57cee7aed8046dca1b53f3e0378ebcfa
Fahri MacBook <fahri-can@web.de>
2021-05-13 00:53:16 +0200

    Rename class and remove other dummy class.

--- app/src/test/java/com/example/apollographqltutorial/repository/CharacterRepositoryImplTest.kt
+++ app/src/test/java/com/example/apollographqltutorial/repository/CharacterRepositoryImplTest.kt
@@ -1,66 +1,96 @@
 package com.example.apollographqltutorial.repository
 
+import com.apollographql.apollo.ApolloCall
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
 import com.example.apollographqltutorial.CharactersListQuery
+import com.example.apollographqltutorial.repository.BaseRepository.Companion.GENERAL_ERROR_CODE
 import com.example.apollographqltutorial.repository.BaseRepository.Companion.SOMETHING_WRONG
+import com.example.apollographqltutorial.util.ResponseFileReader
 import com.example.apollographqltutorial.view.state.ViewState
-import io.mockk.*
-import io.mockk.impl.annotations.RelaxedMockK
+import com.google.gson.Gson
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
+
 @ExperimentalCoroutinesApi
 @RunWith(JUnit4::class)
 class CharacterRepositoryImplTest {
 
-    @RelaxedMockK
-    private lateinit var mockData: CharactersListQuery.Data
+    private val mockWebServer = MockWebServer()
 
+    private lateinit var mockApi: ApolloClient
 
-    @RelaxedMockK
     private lateinit var objectUnderTest: CharacterRepository
 
 
     @Before
     fun setUp() {
-        MockKAnnotations.init(this)
+        mockWebServer.start(8080)
 
-        //objectUnderTest = CharacterRepositoryImpl(mockApolloClient)
+        mockApi = ApolloClient.builder()
+            .serverUrl(mockWebServer.url("/"))
+            .build()
+
+        objectUnderTest = CharacterRepositoryImpl(mockApi)
+    }
+
+    @After
+    fun tearDown() {
+        mockWebServer.shutdown()
     }
 
     @Test
-    fun `given expected success response check if it matches with actual result`() {
-        val expectedSuccess = ViewState.Success(mockData)
-
-        runBlocking {
-            coEvery { objectUnderTest.queryCharactersList() } returns ViewState.Success(mockData)
-
-            val actualResult = objectUnderTest.queryCharactersList()
-
-            coVerify { objectUnderTest.queryCharactersList() }
-
-            assertEquals(expectedSuccess, actualResult)
-        }
-    }
-
-    @Test
-    fun `given expected error response check if it matches with actual result`() {
+    fun `given response failure when fetching results then return exception`() {
         val expectedError = ViewState.Error(Exception(SOMETHING_WRONG))
 
-        runBlocking {
-            coEvery { objectUnderTest.queryCharactersList() } returns expectedError
+        mockWebServer.apply {
+            enqueue(
+                MockResponse().setResponseCode(GENERAL_ERROR_CODE)
+                    .setBody(ResponseFileReader("character_list_error.json").content)
+            )
+        }
 
+        runBlocking {
             val actualResult = objectUnderTest.queryCharactersList()
 
-            coVerify { objectUnderTest.queryCharactersList() }
-
-            assertEquals(expectedError, actualResult)
+            assertEquals(expectedError::class.java, actualResult!!::class.java)
         }
     }
 
+    @Test
+    fun `given response characters list when fetching results then return success`() {
+        val reader = ResponseFileReader("characters_list_success.json")
+        val gson = Gson()
+        val expected: CharactersListQuery.Data =
+            gson.fromJson(reader.content, CharactersListQuery.Data::class.java)
+
+        mockWebServer.apply {
+            enqueue(
+                MockResponse().setResponseCode(GENERAL_ERROR_CODE)
+                    .setBody(ResponseFileReader("characters_list_success.json").content)
+            )
+        }
+
+        runBlocking {
+
+            mockApi.query(CharactersListQuery())
+                .enqueue(object : ApolloCall.Callback<CharactersListQuery.Data>() {
+                    override fun onResponse(response: Response<CharactersListQuery.Data>) {
+                        assertEquals(expected, response.data)
+                    }
+
+                    override fun onFailure(e: ApolloException) {}
+                })
+        }
+    }
 }
