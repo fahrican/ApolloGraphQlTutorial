1fd396c13cb35bd0fcfc792fd40d729c6a468d68
Fahri MacBook <fahri-can@web.de>
2021-05-13 16:34:15 +0200

    Add dummy test for now.

---
+++ app/src/test/java/com/example/apollographqltutorial/viewmodel/CharacterViewModelTest.kt
@@ -0,0 +1,19 @@
+package com.example.apollographqltutorial.viewmodel
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+
+@ExperimentalCoroutinesApi
+@RunWith(JUnit4::class)
+class CharacterViewModelTest {
+
+    @Test
+    fun `dummy test`() {
+        assertEquals(4, 2 + 2)
+    }
+
+}
