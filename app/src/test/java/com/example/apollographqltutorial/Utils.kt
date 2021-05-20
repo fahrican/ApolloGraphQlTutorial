5b83fcf764e02befd5cf815c9570b572aa7af9e6
Fahri MacBook <fahri-can@web.de>
2021-05-20 22:00:33 +0200

    Remove unused code.

--- app/src/test/java/com/example/apollographqltutorial/Utils.kt
+++ app/src/test/java/com/example/apollographqltutorial/Utils.kt
@@ -2,13 +2,10 @@ package com.example.apollographqltutorial
 
 import com.google.common.io.CharStreams
 import okhttp3.mockwebserver.MockResponse
-import java.io.File
-import java.io.FileNotFoundException
 import java.io.IOException
 import java.io.InputStreamReader
 import java.nio.charset.Charset
 import java.util.concurrent.AbstractExecutorService
-import java.util.concurrent.ConcurrentLinkedQueue
 import java.util.concurrent.Executor
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.TimeUnit
@@ -58,48 +55,6 @@ object Utils {
             override fun awaitTermination(l: Long, timeUnit: TimeUnit): Boolean = false
 
             override fun execute(runnable: Runnable) = runnable.run()
-        }
-    }
-
-    class TestExecutor : Executor {
-
-        private val commands = ConcurrentLinkedQueue<Runnable>()
-
-        override fun execute(command: Runnable) {
-            commands.add(command)
-        }
-
-        fun triggerActions() {
-            for (command in commands) {
-                command.run()
-            }
-        }
-    }
-
-    fun checkTestFixture(actualText: String, expectedPath: String) {
-        val expected = File("src/test/fixtures/$expectedPath")
-        val expectedText = try {
-            expected.readText().removeSuffix("\n")
-        } catch (e: FileNotFoundException) {
-            ""
-        }
-
-        expected.parentFile.mkdirs()
-        if (actualText != expectedText) {
-            when (System.getProperty("updateTestFixtures")?.trim()) {
-                "on", "true", "1" -> {
-                    expected.writeText(actualText)
-                }
-                else -> {
-                    throw java.lang.Exception(
-                        """generatedText doesn't match the expectedText.
-      |If you changed the compiler recently, you need to update the testFixtures.
-      |Run the tests with `-DupdateTestFixtures=true` to do so.
-      |generatedText: $actualText
-      |expectedText: $expectedText""".trimMargin()
-                    )
-                }
-            }
         }
     }
 }
